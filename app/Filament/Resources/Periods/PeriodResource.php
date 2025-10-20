6cea8c65bfa02feef5802514aa32b79bbe836f4e
Luis Mayta <luis.mayta@gmail.com>
2025-10-20 12:59:15 -0500

    migraciones

---
+++ app/Filament/Resources/Periods/PeriodResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources\Periods;
+
+use App\Filament\Resources\Periods\Pages\ManagePeriods;
+use App\Models\Period;
+use BackedEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use UnitEnum;
+class PeriodResource extends Resource
+{
+    protected static ?string $model = Period::class;
+    protected static ?string $modelLabel = 'Periodo';
+    protected static ?string $pluralModelLabel = 'Periodos';
+
+    protected static string | UnitEnum | null $navigationGroup = 'Configurar';
+    protected static ?int $navigationSort = 1;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManagePeriods::route('/'),
+        ];
+    }
+}
