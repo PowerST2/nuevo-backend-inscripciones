2234b705a43dc0d09f723b020de7d21cb59f840a
PowerST2 <diegostd99@gmail.com>
2025-10-31 22:13:17 +0000

    adding resources

---
+++ app/Filament/Resources/Majors/MajorResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources\Majors;
+
+use App\Filament\Resources\Majors\Pages\ManageMajors;
+use App\Models\Major;
+use BackedEnum;
+use UnitEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class MajorResource extends Resource
+{
+    protected static ?string $model = Major::class;
+
+    protected static ?string $modelLabel = 'Especialidad';
+    protected static ?string $pluralModelLabel = 'Especialidades';
+
+    protected static string | UnitEnum | null $navigationGroup = 'Configurar';
+    protected static ?int $navigationSort = 2;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('code')
+                    ->required(),
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('channel')
+                    ->required()
+                    ->numeric(),
+                Select::make('faculty_id')
+                    ->relationship('faculty', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                TextColumn::make('code')
+                    ->searchable(),
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('channel')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('faculty.name')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageMajors::route('/'),
+        ];
+    }
+}
