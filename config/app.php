0a23a1fbfcbf0b28c05dba9eae0cf3af8b4b9e09
PowerST2 <diegostd99@gmail.com>
2026-01-13 16:29:23 +0000

    Arreglo url

--- config/app.php
+++ config/app.php
@@ -53,6 +53,8 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'url_simulacro' => 'https://simulacro.admision.uni.edu.pe',
+    
 
     /*
     |--------------------------------------------------------------------------
