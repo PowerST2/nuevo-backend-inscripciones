2a84365a7aa1e9da7c7f913faf73f1d9859cc88d
PowerST2 <diegostd99@gmail.com>
2026-01-24 17:04:25 +0000

    Cambio pago

--- app/Http/Controllers/Api/Simulation/ExamSimulationController.php
+++ app/Http/Controllers/Api/Simulation/ExamSimulationController.php
@@ -12,20 +12,21 @@ class ExamSimulationController extends Controller
     use ExamSimulationTrait;
 
     /**
-     * Obtener estado del simulacro (si hay uno activo o no)
-     * Valida: active=true AND fecha actual entre exam_date_start y exam_date_end
+     * Obtener estado del simulacro (si hay uno activo o no) y si las inscripciones están abiertas.
      */
     public function index()
     {
-        $simulation = $this->getActiveSimulation();
+        $simulation = $this->getActiveSimulation(false); // Traer el simulacro activo sin validar fechas
         $isActive = $simulation !== null;
+        $isInscriptionOpen = $isActive && $simulation->is_within_range; // Inscripción abierta solo si está en rango
 
         // Si está activo, enviar datos
         if ($isActive) {
             return response()->json([
                 'data' => [
                     'status' => 'success',
-                    'is_active' => true,
+                    'is_active' => $isActive,
+                    'is_inscription_open' => $isInscriptionOpen,
                     'description' => $simulation->description,
                     'exam_date_start' => $simulation->exam_date_start->format('d/m/Y'),
                     'exam_date_end' => $simulation->exam_date_end->format('d/m/Y'),
@@ -40,6 +41,7 @@ class ExamSimulationController extends Controller
             'data' => [
                 'status' => 'success',
                 'is_active' => false,
+                'is_inscription_open' => false,
             ]
         ], Response::HTTP_OK);
     }
